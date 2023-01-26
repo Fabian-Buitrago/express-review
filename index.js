a05c37cc37865d211120f1d3da67d3fb1aab0084
Fabian Buitrago <fabianbuitragov@gmail.com>
2023-01-26 17:31:09 -0500

    Review HTTP response concepts

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/products', (req, res) => {
-    // Validate data
-    // Query a database
-    // Process data
-    
-    res.send('product list')
+app.get("/user", (req, res) => {
+  // Validate data
+  // Query a database
+  // Process data
+
+  res.json({
+    name: "Fabian",
+    lastname: "Buitrago",
+    age: 31,
+    points: [1, 2, 3],
+    address: {
+      city: "Bello",
+      street: "Cra 54 # 54 - 10",
+    },
+  });
+});
+
+app.get('/image', (req, res) => {
+    res.sendFile('./javascript.png', {root: __dirname})
 })
 
-app.post('/products', (req, res) => {
-    res.send('creating products')
+app.get('/isAlive', (req, res) => {
+    res.sendStatus(204)
 })
 
-app.put('/products', (req, res) => {
-    res.send('updating products')
-})
+// app.post('/products', (req, res) => {
+//     res.send('creating products')
+// })
 
-app.delete('/products', (req, res) => {
-    res.send('deleting products')
-})
+// app.put('/products', (req, res) => {
+//     res.send('updating products')
+// })
 
-app.patch('/products', (req, res) => {
-    res.send('update a part of the product')
-})
+// app.delete('/products', (req, res) => {
+//     res.send('deleting products')
+// })
 
-app.listen(3000)
-console.log('Server on por ${3000}')
+// app.patch('/products', (req, res) => {
+//     res.send('update a part of the product')
+// })
+
+app.listen(3000);
+console.log("Server on por ${3000}");
