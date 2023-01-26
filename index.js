46127f8ae84bca0bdb6ed14ab9664967fc5b4384
Fabian Buitrago <fabianbuitragov@gmail.com>
2023-01-26 18:31:30 -0500

    review request params

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.text());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  const { id } = req.params;
+  res.send(`New user created: ${id}`);
+});
 
-app.post("/user", (req, res) => {
-  console.log(req.body);
-  res.send("New user created");
+app.get("/add/:value1/:value2", (req, res) => {
+  console.log(req.params);
+  const { value1, value2 } = req.params;
+  res.send(`Result: ${parseInt(value1) + parseInt(value2)}`);
+});
+
+app.get("/users/:username/photo", (req, res) => {
+  console.log(req.params);
+  const { username } = req.params;
+  if (username === "ana") {
+    return res.sendFile("./javascript.png", { root: __dirname });
+  }
+  res.send(`User not found`);
+});
+
+app.get("/name/:name/age/:age", (req, res) => {
+  console.log(req.params);
+  const { name, age } = req.params;
+
+  res.send(`The user ${name} is ${age} years old`);
 });
 
 app.listen(3000);
