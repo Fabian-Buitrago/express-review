273538f02d29c4f45427c2cc2ce074eac07e33f2
Fabian Buitrago <fabianbuitragov@gmail.com>
2023-01-27 17:34:05 -0500

    review middleware part 2 / Logger and Access

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require("express");
 
 const app = express();
 
+//Logger middleware
 app.use((req, res, next) => {
-  console.log(`entered by Middleware, Route: ${req.url} Method: ${req.method}`);
+  //here logger logic
 
+  console.log(`entered by Logger Middleware, Route: ${req.url} Method: ${req.method}`);
   next();
+});
+
+//IsAuthenticated middleware
+app.use((req, res, next) => {
+  //here isAuthenticated logic
+  const { login } = req.query;
+  if (login === "test@test.com") {
+    next();
+  } else {
+    res.send("Not authorized");
+  }
 });
 
 app.get("/profile", (req, res) => {
